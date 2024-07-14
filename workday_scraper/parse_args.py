05d32b3b64124f6e61f16a12612a1e657498b505
Christopher Lam <christopher.peter.lam@gmail.com>
2024-07-14 09:47:26 -0700

    Fix typo

--- workday_scraper/parse_args.py
+++ workday_scraper/parse_args.py
@@ -51,6 +51,6 @@ def parse_args():
     )
     parser.add_argument("-i", "--initial", dest="initial", action="store_true")
     parser.add_argument("-nj", "--no-json", dest="no-json", action="store_true")
-    parser.add_argument("-nr", "--no-json", dest="no-rss", action="store_true")
+    parser.add_argument("-nr", "--no-rss", dest="no-rss", action="store_true")
     args = vars(parser.parse_args())
     return args
